542573a4afafaa006442e70a7bc71a5151a666a5
Oksana Koshel <oksana.koshel@ukr.net>
2023-01-13 20:03:42 +0200

    connect fonts

--- cards/src/App.js
+++ cards/src/App.js
@@ -1,8 +1,7 @@
 import {BrowserRouter} from "react-router-dom";
 import MainComponent from "./components/mainComponent/MainComponent";
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
+
+
 
 function App() {
   return (
--- cards/src/components/resultLine/ResultLine.js
+++ cards/src/components/resultLine/ResultLine.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from "./ResultLine.module.css"
 
 export const ResultLine = ({length}) => {
     return (
-        <div className="results_line">
+        <div className={s.results_line}>
             <p>Results: {length}</p>
             <hr/>
         </div>
